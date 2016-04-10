d237c504e45ca399e540f08cbc68df67524d3f07
dargaCode <darga.code@gmail.com>
2016-04-10 09:27:37 -0700

    Add route for /speak/:animal/
    including a prototype string function to capitalize words

--- app.js
+++ app.js
@@ -15,6 +15,21 @@ const PORT_NUM = 3000;
 const SERVER_START_MSG = "Serving Express Routes Test on port " + PORT_NUM;
 const INDEX_PATH = path.join(__dirname, "/public/index.html");
 
+const ANIMALS = {
+  Bear: "Rawr!",
+  Bird: "Caw!",
+  Cat: "Meow!",
+  Chicken: "b'GAWK!",
+  Darga: "Flomp!",
+  Dog: "Bark!",
+  Elephant: "pfhlaROOOOGH!",
+  Egret: "mmWAAhhh!",
+  Giraffe: "ooh-WAH! ooooh-WAHHHH!",
+  Horse: "eeEEEEEhehehehehe!",
+  Loren: "Burp!",
+  Mouse: "Squeak!"
+}
+
 // SERVER
 
 app.listen(PORT_NUM, function() {
@@ -26,3 +41,18 @@ app.listen(PORT_NUM, function() {
 app.get("/", function(request, response) {
   response.sendFile(INDEX_PATH);
 });
+
+app.get("/speak/:animal", function(request, response) {
+  const animal = request.params.animal.capitalize();
+  const sound = ANIMALS[animal] || "???";
+  const result = `The ${animal} says, "${sound}"`;
+  response.send(result);
+});
+
+// FUNCTIONS
+
+String.prototype.capitalize = function() {
+  const first = this[0].toUpperCase();
+  const remainder  = this.slice(1).toLowerCase();
+  return first + remainder;
+}

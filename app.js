703ae9c85038a1901ba240e463c94d493816bfdd
dargaCode <darga.code@gmail.com>
2016-04-10 05:45:41 -0700

    Set up basic route "/"

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 
 // DEPENDENCIES
 
-var express = require("express");
-var app = express();
+const express = require("express");
+const app = express();
+
+// CONSTANTS
+
+const PORT_NUM = 3000;
+const SERVER_START_MSG = "Serving Express Routes Test on port " + PORT_NUM;
+
+// SERVER
+
+app.listen(PORT_NUM, function() {
+  console.log(SERVER_START_MSG);
+});
+
+// ROUTES
+
+app.get("/", function(request, response) {
+  response.send("hello world!");
+});

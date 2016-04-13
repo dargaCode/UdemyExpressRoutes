b5553db06b75f7a54d90fa0e1a67cc21ccd15848
dargaCode <darga.code@gmail.com>
2016-04-13 16:20:50 -0700

    Set up a simple build system
    - Lint and minify html
    - Lint and minify css

---
+++ Gruntfile.js
@@ -0,0 +1,56 @@
+//wraper function
+module.exports = function(grunt) {
+  //project configuration
+  grunt.initConfig({
+    csslint: {
+      src: ["**/*.css"]
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'public',
+          src: ['*.css', '!*.min.css'],
+          dest: 'public/build',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    htmllint: {
+      your_target: {
+        options: {
+          force: false,
+          "indent-width": 2
+        },
+        src: ["public/index.html"]
+      }
+    },
+
+    htmlmin: {
+      dist: {
+        options: {
+          removeComments: true,
+          collapseWhitespace: true
+        },
+        files: {
+          'public/build/index.min.html': 'public/index.html',
+        }
+      }
+    }
+
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks("grunt-contrib-csslint");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-htmllint");
+  grunt.loadNpmTasks("grunt-contrib-htmlmin");
+
+
+  // Default task(s).
+  grunt.registerTask("all", ["csslint", "cssmin", "htmllint", "htmlmin"]);
+  grunt.registerTask("default", "all");
+
+}

4052fcb5c1ecba5e2cff8d022489dfd141375564
dargaCode <darga.code@gmail.com>
2016-04-18 21:59:10 -0700

    Add Gulp file for comparison to Grunt

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+
+// DEPENDENCIES
+
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var cssmin = require('gulp-cssmin');
+var htmlmin = require('gulp-htmlmin');
+
+// PATHS
+
+var PATHS = {
+  css: ['public/*.css'],
+  html: ['public/*.html'],
+  dest: 'public/build'
+}
+
+// TASKS
+
+gulp.task('css', function() {
+  return gulp.src(PATHS.css)
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(PATHS.dest));
+});
+
+gulp.task('html', function() {
+  return gulp.src(PATHS.html)
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest(PATHS.dest));
+});
+
+gulp.task('watch', function() {
+  gulp.watch(PATHS.css, ['css']);
+  gulp.watch(PATHS.html, ['html']);
+});
+
+gulp.task('default', ['css', 'html'], function() {
+  console.log('DEFAULT TASK');
+});
